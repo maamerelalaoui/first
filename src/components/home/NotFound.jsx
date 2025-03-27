ce7c1789b9fb3f5f58d304f56046489c1473a52d
Ayoub <devpc001@mncprod.com>
2025-03-27 11:57:06 +0000

    Updated WhatsApp 3

--- src/components/home/NotFound.jsx
+++ src/components/home/NotFound.jsx
@@ -6,13 +6,13 @@ import {Home} from "lucide-react";
 const NotFound = () => {
   return (
     <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
-        <div className='w-full md:w-2/3 h-1/2 md:h-2/3 shadow-2xl flex  items-center justify-around'>
+        <div className='w-full md:w-2/3 h-1/2 md:h-2/3 shadow-2xl flex items-center justify-around'>
         <div className='md:ml-14 ml-2'>
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col items-center md:items-start gap-5'>
             <h1 className='text-blue-600 text-6xl font-bold'>Eduverse</h1>
             <p className='font-bold text-xl'>404. <span className='font-medium text-lg text-gray-500'>That's an error.</span></p>
             <div><p className='font-medium text-lg'>The requested URL/404 was not found on this server.</p>
-            <span className='font-medium text-md text-gray-500'>That's all we know.</span></div>
+            <p className='font-medium text-md text-gray-500  text-center md:text-start'>That's all we know.</p></div>
             <div className='flex justify-start'>
                       <Link to="/" className="font-semibold text-lg flex items-center gap-2 group/button">
                         <div className="relative px-6 py-1 flex items-center duration-500 rounded-md bg-blue-600 justify-center overflow-hidden">
